3284068c1e2d33d01930b35b697848fdbdf5eb3a
Taufik Nurrohman <t.nurrohman77@gmail.com>
2016-01-09 10:36:10 +0700

    For Mecha 1.2.x

--- lazy-image-loader/launch.php
+++ lazy-image-loader/launch.php
@@ -7,15 +7,16 @@ if( ! isset($_GET['js']) || (int) $_GET['js'] > 0) {
         $q = $config->url_query;
         $q = strpos($q, '?') === 0 ? str_replace('&', '&amp;', $q) . '&amp;js=0' : '?js=0';
         echo O_BEGIN . '<noscript><meta http-equiv="refresh" content="0;URL=' . $config->url_current . $q . '"></noscript>' . O_END;
+        Config::set('__js_meta_1', true);
     });
 
     // Include the lazy image loader plugin
     Weapon::add('SHIPMENT_REGION_BOTTOM', function() {
-        echo Asset::javascript('cabinet/plugins/' . basename(__DIR__) . '/assets/sword/lazy-image-loader.js');
+        echo Asset::javascript(__DIR__ . DS . 'assets' . DS . 'sword' . DS . 'lazy-image-loader.js');
     });
 
     // Replace `src` attribute with `data-src` on images
-    Filter::add('sanitize:output', function($content) {
+    Filter::add('shield:output', function($content) {
         if( ! Text::check($content)->has('<img ')) {
             return $content;
         }
